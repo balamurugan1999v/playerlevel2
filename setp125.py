015a41c4e3732c4d1c556dce292cc8fc0071ad24
balamurugan1999v <31644214+balamurugan1999v@users.noreply.github.com>
2019-07-07 00:28:59 +0530

    Create setp125.py

---
+++ setp125.py
@@ -0,0 +1,15 @@
+n1=int(input()) 
+b=input().split()
+s1=[]
+#c=1
+for i in b:
+  s1.append(int(i))
+s1.sort()
+for i in range(1,s1[0]+1):
+  temp=0
+  for j in range(n1):
+    if (s1[j]%i)!=0:
+      temp=1
+  if temp==0:
+    c=i
+print(c)
